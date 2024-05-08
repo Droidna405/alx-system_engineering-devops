94d4919ebbaab95a3f0ed50850f36c8f42465228
Droidna405 <joemwangala@gmail.com>
2024-05-08 15:37:12 +0300

    First Commit

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,16 +1,3 @@
 #!/usr/bin/env ruby
-
 # Function to match the regular expression
-def match_school(string)
-  regex = /School/
-
-  match = string.match(regex)
-
-  puts match ? match[0] : ''
-end
-
-if ARGV.empty?
-  puts "Usage: #{$PROGRAM_NAME} <string>"
-else
-  match_school(ARGV[0])
-end
+puts ARGV[0].scan(/school/).join
