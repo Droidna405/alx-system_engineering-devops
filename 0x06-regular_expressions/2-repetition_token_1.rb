ec5bfb36faf9bb6835a9af69d1ef60e73e2626ce
Droidna405 <joemwangala@gmail.com>
2024-05-10 23:19:07 +0300

    Repetition token 2

--- 0x06-regular_expressions/2-repetition_token_1.rb
+++ 0x06-regular_expressions/2-repetition_token_1.rb
@@ -1,15 +1,2 @@
 #!/usr/bin/env ruby
-Check if the argument is provided
-if ARGV.empty?
-  puts "Usage: #{$0} <string>"
-  exit(1)
-end
-
-# Extract the argument
-input_string = ARGV[0]
-
-# Define the regular expression pattern
-pattern = /hb+n/
-
-# Match the pattern against the input string and print the results
-puts input_string.scan(pattern).join("\n")
+puts ARGV[0].scan(/hbt+n/).join
