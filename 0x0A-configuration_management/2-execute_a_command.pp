19daaee487dfede5f17ee90816d608b3a398cda8
Droidna405 <joemwangala@gmail.com>
2024-06-30 22:59:14 +0300

    Terminating a process

---
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,9 @@
+# This Puppet manifest uses the exec resource to kill a process named killmenow using pkill
+
+exec { 'kill_killmenow_process':
+  command     => 'pkill -f killmenow',
+  path        => ['/bin', '/usr/bin', '/usr/local/bin'],
+  refreshonly => false,
+  onlyif      => 'pgrep -f killmenow',
+  }
+  
